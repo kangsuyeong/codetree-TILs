c44d23ba765385c6aa57d3f303b170d974f9d170
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-04-07 16:24:10 +0900

    Title: 점수 비교, Time: 166ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240407/점수 비교/score-comparison.py
@@ -0,0 +1,9 @@
+A = input().split()
+A_math = int(A[0])
+A_english = int(A[1])
+
+B = input().split()
+B_math = int(B[0])
+B_english = int(B[1])
+
+print(int(A_math> B_math and A_english > B_english))
