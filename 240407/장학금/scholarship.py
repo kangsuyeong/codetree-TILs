4e020bd659ce03d5b4661c4fc68ae2b4daf5843b
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-04-07 22:47:09 +0900

    Title: 장학금, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240407/장학금/scholarship.py
+++ 240407/장학금/scholarship.py
@@ -3,12 +3,9 @@ arr = input().split()
 mid = int(arr[0])
 fin = int(arr[1])
 
-if mid >=90:
-    if fin>=95:
-        print(100000)
-    elif fin>=90:
-        print(50000)
-    else :
-        print(0)
-else :
+if mid >=90 and fin >=95:
+    print(100000)
+elif mid>=90 and fin>=90:
+    print(50000)
+else:
     print(0)
