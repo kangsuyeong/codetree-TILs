a9a37f4a53ef0085609096820faffe06ed70fb59
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-04-08 17:08:29 +0900

    Title: 최댓값 구하기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/최댓값 구하기/maximum-value.py
+++ 240408/최댓값 구하기/maximum-value.py
@@ -3,9 +3,13 @@ a=int(arr[0])
 b=int(arr[1])
 c=int(arr[2])
 
-if a>=b and a>=c:
-    print(a)
-elif b>=a and b>=c:
-    print(b)
-else :
-    print(c)
+if a>=b:
+    if a>=c:
+        print(a)
+    else :
+        print(c)
+else:
+    if b>=c:
+        print(b)
+    else:
+        print(c)
