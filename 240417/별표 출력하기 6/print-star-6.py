6e3bf0ad65bad31a0b62d2203b588c03d79c0336
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-04-17 20:23:18 +0900

    Title: 별표 출력하기 6, Time: 168ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240417/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,18 @@
+n=int(input())
+
+for i in range(n):
+    for _ in range(i):
+        print(" ",end=" ")
+    
+    for _ in range(2*n-1-2*i):
+        print("*",end=" ")
+    
+    print("")
+
+for i in range(n-2,-1,-1):
+    for _ in range(i):
+        print(" ",end=" ")
+    for _ in range(2*n-1-2*i):
+        print("*",end=" ")
+
+    print("")
