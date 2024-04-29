6628af59a61079afe5aa33006cf28995376cffe7
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-04-29 21:48:35 +0900

    Title: 제곱하여 출력하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240429/제곱하여 출력하기/print-square-of-elements.py
+++ 240429/제곱하여 출력하기/print-square-of-elements.py
@@ -3,5 +3,6 @@ arr=list(map(int,input().split()))
 
 new_arr=[elem**2 for elem in arr]
 
-for elem in new_arr:
-    print(elem,end=" ")
+# for elem in new_arr:
+#     print(elem,end=" ")
+print(*new_arr)
