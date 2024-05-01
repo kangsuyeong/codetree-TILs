c22b349fd2071fd37d1647b08e5e8cbe96055e9d
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-05-01 15:42:55 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240501/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,15 @@
+n=int(input())
+
+cnt=0
+for _ in range(n):
+    sum_val=0
+    avg=0
+    arr=list(map(int,input().split()))
+    sum_val=sum(arr)
+    avg=sum_val/4
+    if avg>=60:
+        print("pass")
+        cnt+=1;
+    else:
+        print("fail")
+print(cnt)
