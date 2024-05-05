0485cb75389f80d6c6f572feeb57e5988314ac55
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-05-05 15:06:38 +0900

    Title: 대문자로 바꾸기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/대문자로 바꾸기/change-to-capital.py
+++ 240505/대문자로 바꾸기/change-to-capital.py
@@ -2,5 +2,6 @@ arr_2D = [input().split() for i in range(5)]
 
 for i in range(5):
     for j in range(3):
-        print(arr_2D[i][j].upper(),end=" ")
+        arr_2D[i][j]=chr(ord(arr_2D[i][j])+ ord('A')-ord('a'))
+        print(arr_2D[i][j],end=" ")
     print("")
