ba6f50cdfd75652bb62d4d10d60bb350148d0ce5
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-06-03 12:14:43 +0900

    Title: 공백을 기준으로 출력, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240603/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,12 @@
+string1=input()
+string2=input()
+
+for elem in string1:
+    if elem == " ":
+        continue
+    print(elem,end="")
+
+for elem in string2:
+    if elem == " ":
+        continue
+    print(elem,end="")
