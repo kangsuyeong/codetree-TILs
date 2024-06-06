52bc0e984439e14dfd6db0d4ab833e979442a8f2
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-06-06 16:51:34 +0900

    Title: 최소공배수 구하기, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240606/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240606/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,26 +1,9 @@
 n,m=map(int,input().split())
 
 def cal_num(a,b):
-    arr_a=[]
-    arr_b=[]
-    arr_c=[]
-    for i in range(1,a):
-        if a%i ==0:
-            arr_a.append(i)
-    for i in range(1,b):
-        if b%i ==0:
-            arr_b.append(i)
-    for item in arr_a:
-        if item not in arr_b:
-            arr_c.append(item)
-
-    for item in arr_b:
-        if item not in arr_a:
-            arr_c.append(item)
-    final = 1
-    for item in arr_c:
-        final *=item
-    
-    print(final)
+    for i in range(max(a,b),a*b+1):
+        if i%a == 0 and i%b ==0:
+            print(i)
+            break;
 
 cal_num(n,m)
