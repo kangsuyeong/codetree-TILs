ce67686555e6336173a0bd19e63a3f629c489911
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-06-13 10:49:27 +0900

    Title: 첫 번째와 두 번째 교환, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240613/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 240613/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -4,12 +4,11 @@ list_s = list(s)
 s1=list_s[0]
 s2=list_s[1]
 
-list_s[0]=s2
-list_s[1]=s1
-
-for i in range(2,len(list_s)):
+for i in range(len(list_s)):
     if list_s[i] == s1:
-        list_s[i] = s2
+        list_s[i]=s2
+    elif list_s[i] ==s2:
+        list_s[i]=s1
 
 s3=''.join(list_s)
 print(s3)
