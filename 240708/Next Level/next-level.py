621029b9fdb0328440ea545d85090d7b99a70f39
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-08 19:02:19 +0900

    Title: Next Level, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/Next Level/next-level.py
+++ 240708/Next Level/next-level.py
@@ -1,12 +1,19 @@
 class User:
-    def __init__(self,user_id,user_LV):
+    def __init__(self,user_id="",user_LV=0):
         self.user_id=user_id
         self.user_LV=user_LV
     def user_print(self):
         print(f'user {self.user_id} lv {self.user_LV}')
 
+user1 = User()
+user2 = User()
+
+user1.user_id="codetree"
+user1.user_LV=10
 user_id, user_LV = input().split()
-user1 = User("codetree",10)
+user2.user_id=user_id
+user2.user_LV=user_LV
+
 user1.user_print()
-user2 = User(user_id,int(user_LV))
+
 user2.user_print()
