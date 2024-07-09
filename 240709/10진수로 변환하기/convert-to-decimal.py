f7e081e443d9bf83699c4842d903998eb52f30f2
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-09 14:32:42 +0900

    Title: 10진수로 변환하기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240709/10진수로 변환하기/convert-to-decimal.py
+++ 240709/10진수로 변환하기/convert-to-decimal.py
@@ -1,7 +1,13 @@
-n=list(map(int,input()))
+# n=list(map(int,input()))
 
+# num=0
+# for i in range(len(n)):
+#     num=num*2 + n[i]
+
+# print(num)
+
+S=input()
 num=0
-for i in range(len(n)):
-    num=num*2 + n[i]
-
+for digit in S:
+    num=num*2+int(digit)
 print(num)
