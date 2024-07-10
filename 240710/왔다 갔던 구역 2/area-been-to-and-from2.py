d4201bb1539f4b44376d67d7e2dd7e5736a0c3fb
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-10 14:57:08 +0900

    Title: 왔다 갔던 구역 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240710/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,30 @@
+n=int(input())
+OFFSET=1000
+arr = [0]*(2*OFFSET+1)
+# num,D = input().split()
+# num=int(num)
+# print(num,D)
+# current=OFFSET
+# for i in range(current,current+num):
+#     arr[i]+=1
+#     current+=num
+# print(arr[1000])
+
+current=OFFSET
+for _ in range(n):
+    num,D = input().split()
+    num=int(num)
+    if D == "L":
+        for i in range(current-1,current-num-1,-1):
+            arr[i]+=1
+            current-=num
+    else:
+        for i in range(current,current+num):
+            arr[i]+=1
+            current=current+num
+
+total=0
+for elem in arr:
+    if elem>=2:
+        total+=1
+print(total)
