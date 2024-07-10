3568bc25af356d452585b3092a4e760bb8487e7c
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-10 09:12:25 +0900

    Title: 그 요일은, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240710/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,23 @@
+m1,d1,m2,d2 = map(int,input().split())
+
+A=input()
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ["Mon", 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+diff = sum(num_of_days[1:m2])+d2-(sum(num_of_days[1:m1])+d1)
+
+cnt=0
+number = diff-day.index(A)
+
+while True:
+    if number<0:
+        break;
+    elif number==0:
+        cnt+=1
+        break
+    else :
+        number-=7
+        cnt+=1
+
+print(cnt)
