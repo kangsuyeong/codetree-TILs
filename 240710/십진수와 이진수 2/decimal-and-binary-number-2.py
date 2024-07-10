033e381d45d588eb98299c1f432567b7929bccb3
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-10 09:32:23 +0900

    Title: 십진수와 이진수 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240710/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ 240710/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -4,4 +4,15 @@ num=0
 for digits in N:
     num=num*2 + int(digits)
 
-# print(num)
+num=num*17
+
+digits=[]
+while True:
+    digits.append(num%2)
+    num=num//2
+
+    if num==1:
+        digits.append(num)
+        break;
+
+print(*digits[::-1],sep="")
