0e97257ce27c007a3603a796c7f2bd5c8f94e841
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-14 14:51:30 +0900

    Title: 문자에 따른 명령 2, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 240714/문자에 따른 명령 2/text-based-commands2.py
+++ 240714/문자에 따른 명령 2/text-based-commands2.py
@@ -5,11 +5,11 @@ dir_num=0
 
 S=input()
 
-for i in range(len(S)):
-    if S[i]=="F":
+for elem in S:
+    if elem=="F":
         x+=dx[dir_num]
         y+=dy[dir_num]
-    elif S[i]=="L":
+    elif elem=="L":
         dir_num=(dir_num-1)%4
     else:
         dir_num=(dir_num+1)%4
