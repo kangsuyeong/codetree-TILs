ece93b936e4eaef4b5688cf9c80a60733a4d992a
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-14 13:39:33 +0900

    Title: 방향에 맞춰 이동, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/방향에 맞춰 이동/move-in-direction.py
+++ 240714/방향에 맞춰 이동/move-in-direction.py
@@ -9,7 +9,7 @@ dy=[0,0,-1,1]
 for _ in range(N):
     input_D,input_num=input().split()
     input_num=int(input_num)
-    x = x+dx[D.index(input_D)]*input_num
-    y = y+dy[D.index(input_D)]*input_num
+    x +=dx[D.index(input_D)]*input_num
+    y +=dy[D.index(input_D)]*input_num
 
 print(x,y)
