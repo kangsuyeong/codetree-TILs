d87ccb98e460fbeec7b68cf5419e8975e9cdb591
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-15 14:53:03 +0900

    Title: 되돌아오기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/되돌아오기/come-back.py
@@ -0,0 +1,28 @@
+N=int(input())
+OFFSET=1000
+mapper={
+    'E':0,
+    'S':1,
+    'W':2,
+    'N':3
+}
+
+dx,dy=[1,0,-1,0],[0,-1,0,1]
+
+x,y=OFFSET,OFFSET
+
+cnt=0
+for _ in range(N):
+    s,d=input().split()
+    d=int(d)
+    dir_num=mapper[s]
+    for _ in range(d):
+        x+=dx[dir_num]
+        y+=dy[dir_num]
+        cnt+=1
+        if x==OFFSET and y==OFFSET:
+            break;
+    if x==OFFSET and y==OFFSET:
+            break;
+
+print(cnt)
