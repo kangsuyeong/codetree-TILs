b761904dd05067bc03ef474990e22c098601778e
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-16 09:06:25 +0900

    Title: 되돌아오기 2, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/되돌아오기 2/come-back-2.py
+++ 240716/되돌아오기 2/come-back-2.py
@@ -3,10 +3,14 @@ x,y=0,0
 dir_num=0
 dx,dy=[0,1,0,-1],[1,0,-1,0]
 
+ans=-1
+find_zero=False
+cnt=0
 
 def move(cmd):
     global dir_num
     global x,y
+    global ans,find_zero,cnt
     if cmd == "L":
         dir_num=(dir_num-1)%4
     elif cmd=="R":
@@ -14,11 +18,17 @@ def move(cmd):
     else:
         x+=dx[dir_num]
         y+=dy[dir_num]
+    cnt+=1
+    if x==0 and y==0:
+        find_zero=True
+        ans=cnt
 
-ans=0
+    
+    
+
+
 for cmd in string:
     move(cmd)
-    ans+=1
-    if x==0 and y==0:
+    if find_zero:
         break;
 print(ans)
