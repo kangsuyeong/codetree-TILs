364c689e512ff769fe31596923cc15bc66698338
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-16 17:55:45 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240716/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -3,9 +3,8 @@ A=list(input())
 n=len(A)
 cnt=0
 for i in range(n):
-    if A[i]=='(':
         for j in range(i+1,n):
-            if A[j]==')':
+            if A[i]=='(' and A[j]==')':
                 cnt+=1
 
 print(cnt)
