ffb1e8c557a581847404eeb7eea9244dd2c5b06e
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-16 18:10:21 +0900

    Title: 일렬로 서있는 소 2, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240716/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -2,9 +2,9 @@ N=int(input())
 
 A=list(map(int,input().split()))
 ans=0
-for i in range(len(A)):
-    for j in range(i+1,len(A)):
-        for k in range(j+1,len(A)):
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
             if A[i]<=A[j] and A[j]<=A[k]:
                 ans+=1
 
