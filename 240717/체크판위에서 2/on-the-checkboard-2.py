d0d3038c6a112a9431d31a6714a344d2b36d0d93
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-17 10:48:18 +0900

    Title: 체크판위에서 2, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/체크판위에서 2/on-the-checkboard-2.py
+++ 240717/체크판위에서 2/on-the-checkboard-2.py
@@ -5,8 +5,7 @@ arr=[
     for _ in range(R)
 ]
 
-jump1=0
-jump2=0
+
 
 cnt=0
 for i in range(1,R):
@@ -14,7 +13,7 @@ for i in range(1,R):
         if arr[0][0]!=arr[i][j]:
             for k in range(i+1,R-1):
                 for l in range(j+1,C-1):
-                    if arr[i][j]!=arr[k][l]:
+                    if arr[i][j]!=arr[k][l] and arr[k][l]!=arr[R-1][C-1]:
                         cnt+=1
 
 print(cnt)
