8fe9bc6df99145679eeadd00ac02295436e28db1
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-17 21:28:02 +0900

    Title: 최고의 13위치 2, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/최고의 13위치 2/best-place-of-13-2.py
+++ 240717/최고의 13위치 2/best-place-of-13-2.py
@@ -12,17 +12,11 @@ max_num=0
 for i in range(N):
     for j in range(N-2):
         for k in range(i,N):
-            if i==k:
-                for l in range(j+3,N-2):
-                    if in_range(l) and in_range(l+1) and in_range(l+2):
-                        ans=0
-                        ans+=arr[i][j]+arr[i][j+1]+arr[i][j+2]
-                        ans+=arr[k][l]+arr[k][l+1]+arr[k][l+2]
-                    max_num=max(max_num,ans)
-            else :
-                for l in range(N-2):
-                    ans=0
-                    ans+=arr[i][j]+arr[i][j+1]+arr[i][j+2]
-                    ans+=arr[k][l]+arr[k][l+1]+arr[k][l+2]
-                    max_num=max(max_num,ans)
+            for l in range(N-2):
+                if i==k and (l-j)<3:
+                    continue
+                ans=0
+                ans+=arr[i][j]+arr[i][j+1]+arr[i][j+2]
+                ans+=arr[k][l]+arr[k][l+1]+arr[k][l+2]
+                max_num=max(max_num,ans)
 print(max_num)
