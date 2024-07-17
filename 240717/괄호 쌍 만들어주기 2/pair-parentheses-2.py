3f901285faf7b1dbae4c9ea9aa4c5891b34c84c4
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-17 19:06:26 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240717/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -2,7 +2,7 @@ A=input()
 n=len(A)
 ans=0
 for i in range(n-1):
-    for j in range(n-1):
+    for j in range(i+2,n-1):
         if A[i]=='(' and A[i+1]=='(' and A[j]==')' and A[j+1]==')':
             ans+=1
 
