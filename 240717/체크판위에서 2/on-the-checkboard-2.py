6a9870ac5a9412d0484fb5f9ce57c858a3f2e568
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-17 10:37:46 +0900

    Title: 체크판위에서 2, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240717/체크판위에서 2/on-the-checkboard-2.py
+++ 240717/체크판위에서 2/on-the-checkboard-2.py
@@ -9,8 +9,8 @@ jump1=0
 jump2=0
 
 cnt=0
-for i in range(1,R-1):
-    for j in range(1,C-1):
+for i in range(1,R):
+    for j in range(1,C):
         if arr[0][0]!=arr[i][j]:
             for k in range(i+1,R-1):
                 for l in range(j+1,C-1):
