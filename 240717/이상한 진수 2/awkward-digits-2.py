45a375dcb6f220d0e1fa84234727e23c06c76098
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-17 11:53:38 +0900

    Title: 이상한 진수 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/이상한 진수 2/awkward-digits-2.py
+++ 240717/이상한 진수 2/awkward-digits-2.py
@@ -2,11 +2,11 @@ a=list(map(int,input()))
 
 max_num=0
 for i in range(len(a)):
-    a[i]=(a[i]+1)%2
+    a[i]=1-a[i]
     S=0
     for digit in a:
         S=S*2 + digit
     max_num=(max(max_num,S))
-    a[i]=(a[i]+1)%2
+    a[i]=1-a[i]
 
 print(max_num)
