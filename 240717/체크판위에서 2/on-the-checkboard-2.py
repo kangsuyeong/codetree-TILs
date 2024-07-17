3b6369acb85b3d034d4ea27a9f5de4bbf2d0baf2
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-17 10:31:12 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240717/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,20 @@
+R,C=map(int,input().split())
+
+arr=[
+    list(input().split())
+    for _ in range(R)
+]
+
+jump1=0
+jump2=0
+
+cnt=0
+for i in range(1,C-1):
+    for j in range(1,R-1):
+        if arr[0][0]!=arr[i][j]:
+            for k in range(i+1,C-1):
+                for l in range(j+1,C-1):
+                    if arr[i][j]!=arr[k][l]:
+                        cnt+=1
+
+print(cnt)
