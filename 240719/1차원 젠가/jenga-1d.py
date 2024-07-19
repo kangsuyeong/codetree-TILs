6e5ee1595caace22bd915d9c5dee364409982907
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-19 20:19:24 +0900

    Title: 1차원 젠가, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/1차원 젠가/jenga-1d.py
+++ 240719/1차원 젠가/jenga-1d.py
@@ -1,28 +1,44 @@
-n=int(input())
+#A[s] ~ A[e]를 뺀다. => 겨로가 리스트 반환
+def f(A,s,e):
+    temp=[]
+    for i in range(len(A)):
+        if s<=i<=e:
+            continue
+        temp.append(A[i])
+    return temp
 
-arr=[int(input()) for _ in range(n)]
+N=int(input())
 
-s1,e1=map(int,input().split())
-s2,e2=map(int,input().split())
-temp=[]
+A=[int(input()) for _ in range(N)]
 
-for i in range(s1-1,e1):
-    arr[i]=0
+for _ in range(2):
+    s,e=map(int,input().split())
+    A=f(A,s-1,e-1)
 
-for i in range(n):
-    if arr[i]!=0:
-        temp.append(arr[i])
+print(len(A))
+print(*A, sep="\n")
 
-arr=temp
-temp=[]
+# s1,e1=map(int,input().split())
+# s2,e2=map(int,input().split())
+# temp=[]
 
-for i in range(s2-1,e2):
-    arr[i]=0
+# for i in range(s1-1,e1):
+#     arr[i]=0
 
-for i in range(len(arr)):
-    if arr[i]!=0:
-        temp.append(arr[i])
+# for i in range(n):
+#     if arr[i]!=0:
+#         temp.append(arr[i])
 
-print(len(temp))
-for elem in temp:
-    print(elem)
+# arr=temp
+# temp=[]
+
+# for i in range(s2-1,e2):
+#     arr[i]=0
+
+# for i in range(len(arr)):
+#     if arr[i]!=0:
+#         temp.append(arr[i])
+
+# print(len(temp))
+# for elem in temp:
+#     print(elem)
