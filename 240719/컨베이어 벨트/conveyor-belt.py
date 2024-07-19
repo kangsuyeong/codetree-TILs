4bb06b145f93b1e13add727ee5fb504d0e544192
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-19 16:24:58 +0900

    Title: 컨베이어 벨트, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/컨베이어 벨트/conveyor-belt.py
+++ 240719/컨베이어 벨트/conveyor-belt.py
@@ -6,9 +6,8 @@ arr+=list(map(int,input().split()))
 
 
 for _ in range(t):
-    temp=arr[len(arr)-1]
-    for i in range(n*2-1,0,-1):
-        arr[i]=arr[i-1]
+    temp=arr[-1]
+    arr[1:2*n]=arr[0:2*n-1]
     arr[0]=temp
 
 for i in range(2*n):
