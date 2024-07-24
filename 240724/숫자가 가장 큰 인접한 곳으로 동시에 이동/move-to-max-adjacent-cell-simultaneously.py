300a7e1050dc73c5f8be931057a10d4290583de9
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-24 20:07:18 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 161ms, Memory: 28MB, Status: Passed - CodeTree

--- 240724/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240724/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,36 +1,36 @@
 n,m,t=map(int,input().split())
 
+# 기존배열
 arr=[[0]*(n+1)]
 for _ in range(n):
     arr.append([0]+list(map(int,input().split())))
-
-Count = [[0]*(n+1)
-    for _ in range(n+1)
-]
-new_Count = [
+# 위치배열
+count = [
     [0]*(n+1)
     for _ in range(n+1)
 ]
-# 카운트에 채우기
+
 for _ in range(m):
     r,c=map(int,input().split())
-    Count[r][c]=1
+    count[r][c]=1
 
-# dx,dy
-dxs,dys=[-1,1,0,0],[0,0,-1,1]
+# 위치 이동배열
+next_count=[
+    [0]*(n+1)
+    for _ in range(n+1)
+]
 
 def in_range(x,y):
     return 0<x<=n and 0<y<=n
 
-def simulate():
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            new_Count[i][j]=0
 
-    global Count,arr
+# 위치 이동시키기
+
+def move():
+    dxs,dys=[-1,1,0,0],[0,0,-1,1]
     for i in range(1,n+1):
         for j in range(1,n+1):
-            if Count[i][j]==1:
+            if count[i][j]==1:
                 max_num=0
                 max_x,max_y=0,0
                 for dx,dy in zip(dxs,dys):
@@ -38,26 +38,32 @@ def simulate():
                     if in_range(nx,ny) and max_num<arr[nx][ny]:
                         max_num=arr[nx][ny]
                         max_x,max_y=nx,ny
-                new_Count[max_x][max_y]+=1           
-    
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            Count[i][j]=new_Count[i][j]
 
+                next_count[max_x][max_y]+=1
 
 def remove_dup():
     for i in range(1,n+1):
         for j in range(1,n+1):
-            if Count[i][j]>=2:
-                Count[i][j]=0
+            if next_count[i][j]>=2:
+                next_count[i][j]=0
+
+
+
 
 for _ in range(t):
-    simulate()
+    move()
     remove_dup()
+    # 복사
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            count[i][j]=next_count[i][j]
+    # 초기화
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            next_count[i][j]=0
 
-
+# 최종결과
 s=0
-
-for row in Count:
-    s+=sum(row)
+for elem in count:
+    s+=sum(elem)
 print(s)
