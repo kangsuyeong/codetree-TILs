33e12981492bcc3999e1e1d2f49f8c093c44f627
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-07-24 11:42:31 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 194ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240724/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240724/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -42,7 +42,7 @@ def remove_dup():
     for i in range(1,n+1):
         for j in range(1,n+1):
             if Count[i][j]>=2:
-                Count[i][j]=1
+                Count[i][j]=0
 
 for _ in range(t):
     simulate()
