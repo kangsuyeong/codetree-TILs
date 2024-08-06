f36bc90b0e61ebbcba638fb8f62c2a73b7363f86
kangsuyeong <127009652+kangsuyeong@users.noreply.github.com>
2024-08-06 11:42:44 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 267ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240806/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,38 @@
+# # 재귀 함수 - 함수 정의
+# # get_sum(n) : 1~ n 까지의 합을 반환하는 함수
+
+# n=3
+# answer=[]
+
+# def choose(curr_num):
+#     # 종료조건(초기값)
+#     if curr_num == n+1:
+#         print(*answer,sep="")
+#         return
+    
+#     #다음 내용 호출
+
+#     for i in range(3):
+#         answer.append(i)
+#         choose(curr_num+1)
+#         answer.pop()
+
+
+
+
+# choose(1)
+
+K,N= map(int,input().split())
+answer=[]
+
+def choose(curr_num):
+    if curr_num==N+1:
+        print(*answer,sep=" ")
+        return
+
+    for i in range(1,K+1):
+        answer.append(i)
+        choose(curr_num+1)
+        answer.pop()
+
+choose(1)
