114d5e21e853fb9305ff0ae790b7d147f08efe93
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2024-08-06 21:48:51 +0900

    Title: 크기가 n인 순열, Time: 226ms, Memory: 29MB, Status: Passed - CodeTree

--- 240806/크기가 n인 순열/n-permutation.py
+++ 240806/크기가 n인 순열/n-permutation.py
@@ -1,4 +1,32 @@
+# n=int(input())
+# answer=[]
+# visited=[False]*(n+1)
+
+# def choose(curr_num):
+#     if curr_num==n+1:
+#         print(*answer,sep=" ")
+#         return
+    
+#     # 순열돌기
+#     for i in range(1,n+1):
+#         if visited[i]:
+#             continue
+        
+#         visited[i] = True
+#         answer.append(i)
+
+#         choose(curr_num+1)
+        
+#         visited[i]=False
+#         answer.pop()
+        
+        
+
+# choose(1)
+
+
 n=int(input())
+
 answer=[]
 visited=[False]*(n+1)
 
@@ -7,19 +35,14 @@ def choose(curr_num):
         print(*answer,sep=" ")
         return
     
-    # 순열돌기
     for i in range(1,n+1):
         if visited[i]:
             continue
         
-        visited[i] = True
+        visited[i]=True
         answer.append(i)
-
         choose(curr_num+1)
-        
-        visited[i]=False
         answer.pop()
-        
-        
+        visited[i]=False
 
 choose(1)
