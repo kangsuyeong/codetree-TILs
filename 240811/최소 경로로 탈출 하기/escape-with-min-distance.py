d51d3756055b3320386489f8e6822831ab1bab5b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2024-08-11 17:49:09 +0900

    Title: 최소 경로로 탈출 하기, Time: 196ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240811/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,83 @@
+# from collections import deque
+# n,m=map(int,input().split())
+
+# grid = [
+#     list(map(int,input().split()))
+#     for _ in range(n)
+# ]
+# q=deque()
+
+# visited = [
+#     [False for _ in range(m)]
+#     for _ in range(n)
+# ]
+# step=[
+#     [-1 for _ in range(m)]
+#     for _ in range(n)
+# ]
+
+# def in_range(x,y):
+#     return 0<=x<n and 0<=y<m
+
+# def can_go(x,y):
+#     return in_range(x,y) and grid[x][y]==1 and not visited[x][y]
+
+# def push(x,y,s):
+#     step[x][y]=s
+#     visited[x][y] = True
+#     q.append((x,y))
+
+
+# def bfs():
+#     while q:
+#         x,y=q.popleft()
+#         dxs,dys=[0,1,0,-1],[1,0,-1,0]
+#         for dx,dy in zip(dxs,dys):
+#             nx,ny=x+dx,y+dy
+#             if can_go(nx,ny):
+#                 push(nx,ny,step[x][y]+1)
+
+# push(0,0,0)
+# bfs()
+# print(step)
+# print(step[-1][-1])
+
+from collections import deque
+n,m=map(int,input().split())
+
+grid=[
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+q=deque()
+visited=[
+    [False for _ in range(m)]
+    for _ in range(n)
+]
+
+step=[
+    [-1 for _ in range(m)]
+    for _ in range(n)
+]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<m
+
+def push(x,y,s):
+    visited[x][y]=True
+    q.append((x,y))
+    step[x][y]=s
+
+
+def bfs():
+    while q:
+        x,y=q.popleft()
+        dxs,dys=[0,1,0,-1],[1,0,-1,0]
+        for dx,dy in zip(dxs,dys):
+            nx,ny=x+dx,y+dy
+            if in_range(nx,ny) and grid[nx][ny]==1 and not visited[nx][ny]:
+                push(nx,ny,step[x][y]+1)
+push(0,0,0)
+bfs()
+print(step[-1][-1])
